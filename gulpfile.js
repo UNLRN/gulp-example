868ec5fe460289d239cc7618c95b36c34619c965
Matt Jiles <mjiles1@gmail.com>
2017-05-05 20:58:24 -0400

    add autoprefixer and cssbeautify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const cssbeautify = require('gulp-cssbeautify');
 
 gulp.task('styles', function () {
 	gulp.src('./scss/main.sass')
 	.pipe(sass())
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(cssbeautify({
+		indent: '    ',
+		autosemicolon: true
+	}))
 	.pipe(gulp.dest('./css'))
 	.pipe(browserSync.reload({stream: true}));
 });
